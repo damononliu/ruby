8583f93e64422a1410688a4458dc83df490c44f1
Damon <liupengfei16@mails.ucas.ac.cn>
2016-11-21 06:06:27 +0000

    fun_with_strings.rb first commit

---
+++ fun_with_strings.rb
@@ -0,0 +1,41 @@
+module FunWithStrings
+  def palindrome?
+    # your code here
+    str = self.downcase.gsub(/[^A-Za-z]/,"")
+    if (str.reverse == str)
+    	true
+    else
+    	false
+    end
+  end
+  def count_words
+   # your code here
+  str = self.downcase.gsub(/[^A-Za-z]/," ")
+  count = Hash.new(0)
+  words = str.split(" ")
+  words.each  do |word|
+  count[word] += 1
+  end
+  return count
+  end
+  def anagram_groups
+   # your code here
+   hash = Hash.new
+   str = self.split(" ")
+   str.each do |word|
+     key = word.chars.sort.join
+     if hash[key]
+       hash[key] << word
+     else
+       hash[key] = [word]
+     end
+   end
+   return hash.values
+  end
+end
+
+# make all the above functions available as instance methods on Strings:
+
+class String
+  include FunWithStrings
+end
